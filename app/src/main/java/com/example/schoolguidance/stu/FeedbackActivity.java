1f9b01ef87c8f14784bf1587d755688471e624bb
ActiveChai <1078486770@qq.com>
2019-05-12 21:12:44 +0800

    feedback完成

--- app/src/main/java/com/example/schoolguidance/stu/FeedbackActivity.java
+++ app/src/main/java/com/example/schoolguidance/stu/FeedbackActivity.java
@@ -1,6 +1,8 @@
 package com.example.schoolguidance.stu;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.schoolguidance.R;
+import com.example.schoolguidance.tool.HttpTool;
 import com.qmuiteam.qmui.widget.dialog.QMUITipDialog;
 import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 
@@ -18,6 +21,26 @@ public class FeedbackActivity extends AppCompatActivity {
     QMUIRoundButton btn_submit_feed;
     TextView const_text_feed;
 
+    private static final int MESS_WHAT_TEST_POST = 301;
+
+    private HttpTool httpTool;
+
+    final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            System.out.println(msg);
+
+            switch (msg.what) {
+                case MESS_WHAT_TEST_POST:
+                    System.out.println("post ok");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,26 +48,37 @@ public class FeedbackActivity extends AppCompatActivity {
         init();
     }
 
-    public void init(){
+    public void init() {
         const_text_feed = (TextView) findViewById(R.id.const_text_feed);
         btn_submit_feed = (QMUIRoundButton) findViewById(R.id.btn_submit_feed);
         content_feed = (EditText) findViewById(R.id.content_feed);
     }
 
 
-    public void onClick(View view){
+    public void onClick(View view) {
         Intent intent = new Intent();
         switch (view.getId()) {
             case R.id.btn_submit_feed:
                 String text = content_feed.getText().toString();
-                if(text.equals("")){
-                    final QMUITipDialog tipDialog= new QMUITipDialog.Builder(this)
+                if (text.equals("")) {
+                    final QMUITipDialog tipDialog = new QMUITipDialog.Builder(this)
                             .setIconType(QMUITipDialog.Builder.ICON_TYPE_FAIL)
                             .setTipWord("输入为空")
                             .create();
                     tipDialog.show();
-                    const_text_feed.postDelayed(new Runnable() { @Override public void run() { tipDialog.dismiss(); } }, 1000);
+                    const_text_feed.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            tipDialog.dismiss();
+                        }
+                    }, 1000);
+                } else {
+                    httpTool = new HttpTool(HttpTool.MODE_POST, "http://129.211.28.150:8443/api/addfeed", MESS_WHAT_TEST_POST, handler);
 
+                    httpTool.clearData();
+                    httpTool.addData("content_feed", content_feed.getText().toString());
+
+                    httpTool.start();
                 }
                 break;
 
