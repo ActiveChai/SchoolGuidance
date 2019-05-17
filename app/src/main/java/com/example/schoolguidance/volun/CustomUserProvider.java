f1eb20c9d73e3600740c50971ab20a7e21812760
aobi12138 <443017511@qq.com>
2019-05-17 23:48:10 +0800

    条形图和聊天页面

---
+++ app/src/main/java/com/example/schoolguidance/volun/CustomUserProvider.java
@@ -0,0 +1,56 @@
+package com.example.schoolguidance.volun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.leancloud.chatkit.LCChatKitUser;
+import cn.leancloud.chatkit.LCChatProfileProvider;
+import cn.leancloud.chatkit.LCChatProfilesCallBack;
+
+/**
+ * Created by wli on 15/12/4.
+ * 实现自定义用户体系
+ */
+public class CustomUserProvider implements LCChatProfileProvider {
+
+  private static CustomUserProvider customUserProvider;
+
+  public synchronized static CustomUserProvider getInstance() {
+    if (null == customUserProvider) {
+      customUserProvider = new CustomUserProvider();
+    }
+    return customUserProvider;
+  }
+
+  private CustomUserProvider() {
+  }
+
+  private static List<LCChatKitUser> partUsers = new ArrayList<LCChatKitUser>();
+
+  // 此数据均为 fake，仅供参考
+  static {
+    partUsers.add(new LCChatKitUser("志愿者007", "志愿者007", "emoji_unselected.png"));
+    partUsers.add(new LCChatKitUser("新生2019", "新生2019", "emoji_unselected.png"));
+    partUsers.add(new LCChatKitUser("新生2018", "新生2018", "emoji_unselected.png"));
+
+  }
+
+  @Override
+  public void fetchProfiles(List<String> list, LCChatProfilesCallBack callBack) {
+    List<LCChatKitUser> userList = new ArrayList<LCChatKitUser>();
+    for (String userId : list) {
+      for (LCChatKitUser user : partUsers) {
+        if (user.getUserId().equals(userId)) {
+          userList.add(user);
+          break;
+        }
+      }
+    }
+    callBack.done(userList, null);
+  }
+
+  @Override
+  public List<LCChatKitUser> getAllUsers() {
+    return partUsers;
+  }
+}
--- app/src/main/java/com/example/schoolguidance/volun/VolunMainActivity.java
+++ app/src/main/java/com/example/schoolguidance/volun/VolunMainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,22 +14,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.schoolguidance.R;
-import com.example.schoolguidance.stu.AskHelpActivity;
-import com.example.schoolguidance.stu.FeedbackActivity;
-import com.example.schoolguidance.stu.MapActivity;
 import com.example.schoolguidance.stu.MessagesActivity;
-import com.example.schoolguidance.stu.PunchCardsActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import cn.leancloud.chatkit.LCChatKitUser;
 
 public class VolunMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ViewPager viewPager;
+    private TextView emptyText;
+    private ListView task_lisk_main;
+    static public List<Map<String, Object>> taskList = new ArrayList<Map<String, Object>>();
+    static public SimpleAdapter adapter = null;
+    static public List<LCChatKitUser> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.volunteer_main);
+        task_lisk_main = (ListView) findViewById(R.id.listView_volun_main);
+        emptyText = (TextView) findViewById(R.id.textView_empty);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        userList = CustomUserProvider.getInstance().getAllUsers();
+        adapter = new SimpleAdapter(this, taskList, R.layout.item_tasklist_volun_main,
+                new String[] {  },
+                new int[] { });
+        task_lisk_main.setAdapter(adapter);
+        task_lisk_main.setEmptyView(emptyText);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -48,6 +69,8 @@ public class VolunMainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
